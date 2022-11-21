f7ced9c07b108463fcd86ab685b0e409becb6192
faraz7321 <faraz7321@gmail.com>
2022-11-21 22:55:44 +0100

    init

---
+++ ReadMe.txt
@@ -0,0 +1,20 @@
+Google Colab: https://colab.research.google.com/drive/1MEP59MF5hHvQMyfxJXglxgVy5iLYFS-7?usp=sharing
+Note: you will need to upload the image which needs to be proccessed
+This example uses the open source library "AprilTag"
+github: https://github.com/AprilRobotics/apriltag
+
+
+Another example is based on the AprilTag python bindings created by duckietown.
+github: https://github.com/duckietown/lib-dt-apriltags
+you can install it with pip
+	$ pip install dt-apriltags
+
+
+I also tested apriltag_ros which is ROS wrapper of the AprilTag 3 visual fiducial detector.
+to use it, i simply created my catkin workspace, cloned package into my src directory
+and built it. I was able to use it with 
+	$ roslaunch apriltag_ros single_image_server.launch 
+	$ roslaunch apriltag_ros single_image_client.launch image_load_path:=<absolute_path>
+	  image_save_path:=<absolute_path>
+but before, I needed to configure tags.yaml with the standalone tags and tag bundles which we want to detect and settings.yaml with the wrapper and apriltag core parameters. *I am still working on it*
+
---
+++ duckietown_at.py
@@ -0,0 +1,52 @@
+# %%
+# Import libraries
+
+from dt_apriltags import Detector
+import cv2
+
+# %%
+# using duckietown based apriltags detector
+
+at_detector = Detector(families='tag36h11',
+                       nthreads=1,
+                       quad_decimate=1.0,
+                       quad_sigma=0.0,
+                       refine_edges=1,
+                       decode_sharpening=0.25,
+                       debug=0)
+
+# %%
+print("\nTESTING WITH A SAMPLE IMAGE")
+
+img = cv2.imread("image.png")  # colored image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # grayscale image
+
+# %%
+tags = at_detector.detect(gray)
+print(tags)
+
+# %%
+# add tags in image
+for tag in tags:
+
+    # draw border lines
+    for idx in range(len(tag.corners)):
+        cv2.line(img, tuple(
+            tag.corners[idx-1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0))
+
+    # add text in image
+    cv2.putText(img, "Tag " + str(tag.tag_id),
+                org=(tag.center[0].astype(int),
+                     tag.center[1].astype(int)),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.5,
+                color=(0, 255, 0))
+
+# %%
+cv2.imshow('Detected tags', img)
+cv2.imwrite('output.png', img)
+
+# %%
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
